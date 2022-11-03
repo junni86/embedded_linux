2c397b61f5c8a2b7e4fb6047eb5dc6119cfdae86
jaekun86 <literary86@naver.com>
2022-11-03 04:48:14 +0000

    {test}

---
+++ day4/18_sjlee/06_linux_kernal_module_GPIO/.led_module.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/test/day4/18_sjlee/06_linux_kernal_module_GPIO/led_module.mod := { echo  /home/pi/test/day4/18_sjlee/06_linux_kernal_module_GPIO/led_module.o;  echo; } > /home/pi/test/day4/18_sjlee/06_linux_kernal_module_GPIO/led_module.mod
